b6424631db5496479f8f97e4a9392eaed518c11f
Sam Asghari <sam.asghari@gatech.edu>
2011-06-29 15:04:03 -0400

    dynamically retreive comments and submitted stories for a reddit user

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,19 @@
 /**
- * returns a string containing a desired representaiton for an epoch time
+ * Object representing a redditor and 
+ * related important information
+ */
+function Redditor()
+{
+    this.username = "";
+    this.comments = new Array();
+	this.comments_after = "";
+	this.submitted = new Array();
+	this.submitted_after = "";
+	this.debug = true;
+}
+
+/**
+ * A function to format an epoch datetime
  * @param utc - an integer representing a utc time
  * @returns - the desired date representation
  */
